f44e5e932cda8412f1ea24baf1875480faf84f93
LARATEK <58705098+LARATEK@users.noreply.github.com>
2023-12-26 21:13:18 +0300

    хочу нюху

--- Module/Generator.cpp
+++ Module/Generator.cpp
@@ -30,7 +30,10 @@ void Generator::Process(Tree* tree)
 		}
 	}
 	if (data == "const") _const = tree->GetLeft();
-	if (data == "var") _var = tree->GetLeft(); 
+	if (_var == nullptr)
+	{
+		if (data == "var") _var = tree->GetLeft();
+	}
 	if (data == "procedure") _procedure = tree->GetLeft();
 }
 
@@ -72,6 +75,11 @@ void Generator::Operator(Tree* tree)
 	if (tree->GetLeft() != nullptr) Operator(tree->GetLeft());
 	if (tree->GetRight() != nullptr) Operator(tree->GetRight());
 	OperatorProcess(tree);
+	if (tree->GetData() == "exit")	
+	{
+		cout << "jmp exit" << endl;
+		exit_flag = 1;
+	}
 }
 
 void Generator::OperatorProcess(Tree* tree)
@@ -99,12 +107,11 @@ void Generator::OperatorProcess(Tree* tree)
 				cout << "mul BX" << endl;
 				cout << "push AX" << endl;
 			}
-			if (tree->GetData() == ":=")		//pop id
+			if (tree->GetData() == ":=")
 			{
-				int index = stoi(tree->GetLeft()->GetData().substr(2));
-				cout << "pop AX" << endl;
+				int index = stoi(tree->GetLeft()->GetData().substr(2)); 
+				cout << "pop " << ids->At(index).GetContent() << endl;
 				cout << "pop BX" << endl;
-				cout << "mov " << ids->At(index).GetContent() << ", AX" << endl;
 			}
 			if (tree->GetData() == "-")
 			{
@@ -123,34 +130,96 @@ void Generator::OperatorProcess(Tree* tree)
 		}
 		else
 		{
-			if ((tree->GetData() != "NULL") and (tree->GetData().substr(0, 2) != "OP") and (tree->GetData() != "exit"))
+			if ((tree->GetData() != "NULL") and (tree->GetData().substr(0, 2) != "OP") and (tree->GetData() != "exit") and (tree->GetData().substr(0, 4) != "PROC"))
 			{
 				cout << "push " << tree->GetData() << endl;
 			}
 		}
 	}	
-	if (tree->GetData() == "exit")
-	{
-		cout << "jmp exit" << endl;
-		exit_flag = 1;
-	}
-}
-
-void Generator::ProcedureTreeTraversal(Tree* tree)		
-{
 	
 }
 
-void Generator::Procedure(Tree* tree)	//нужно подумать о порядке обхода
+void Generator::ProcedureTreeTraversal(Tree* tree)	//обход возможно неправильный		
 {
-	if (tree->GetLeft() != nullptr) Procedure(tree->GetLeft());
-	if (tree->GetRight() != nullptr) Procedure(tree->GetRight());
-	ProcedureProcess(tree);
+	ProcedureAddress(tree);
+	if (tree->GetLeft() != nullptr) ProcedureTreeTraversal(tree->GetLeft());
+	if (tree->GetRight() != nullptr) ProcedureTreeTraversal(tree->GetRight());
 }
 
-void Generator::ProcedureProcess(Tree* tree)	//наверное нужно работать как с основной прогой
+void Generator::ProcedureAddress(Tree* tree)
 {
+	if ((tree->GetData().substr(0, 2) == "PR") and (tree->GetData().substr(0, 4) != "PROC"))
+	{
+		procedure_address[pr_count_proc] = tree->GetLeft();
+		pr_count_proc += 1;
+	}
+}
 
+void Generator::Procedure(Tree* tree)
+{
+	ProcedureAddressInside(tree);
+	if (tree->GetLeft() != nullptr) Procedure(tree->GetLeft());
+	if (tree->GetRight() != nullptr) Procedure(tree->GetRight());
+}
+
+void Generator::ProcedureAddressInside(Tree* tree)	
+{
+	if (tree->GetData() == "var")
+	{
+		var_proc[pr_count_var] = tree->GetLeft();
+		pr_count_var += 1;
+	}
+	if (tree->GetData() == "block")
+	{
+		block_proc[pr_count_block] = tree->GetRight();
+		pr_count_block += 1;
+	}
+}
+
+void Generator::ProcedureProcess(Tree* tree, int i)	
+{
+	int index = stoi(tree->GetData().substr(2));
+	cout << ids->At(index).GetContent() << ":" << endl;	//нужно засунуть все используемые переменные в стек, а потом вытащить в обратном порядке
+	Tree* var_pr = var_proc[i];
+	ProcedureVar(var_pr);
+	Tree* block_pr = block_proc[i];
+	ProcedureBlock(block_pr, i);
+	if (exit_flag_proc == 1)
+	{
+		cout << "exit_proc_" << i << ":" << endl;
+		exit_flag_proc = 0;
+	}
+	cout << stack_out;
+	cout << "ret" << endl;
+}
+
+void Generator::ProcedureVar(Tree* tree)
+{
+	ProcedureVarProcess(tree);
+	if (tree->GetLeft() != nullptr) ProcedureVar(tree->GetLeft());
+	if (tree->GetRight() != nullptr) ProcedureVar(tree->GetRight());
+}
+
+void Generator::ProcedureVarProcess(Tree* tree)
+{
+	if (tree->GetData().substr(0, 2) == "id")
+	{
+		int index = stoi(tree->GetData().substr(2));
+		cout << "push " << ids->At(index).GetContent() << endl;
+		stack_out ="pop " + ids->At(index).GetContent() + "\n" + stack_out;
+ 	}
+}
+
+void Generator::ProcedureBlock(Tree* tree, int i)
+{
+	if (tree->GetLeft() != nullptr) ProcedureBlock(tree->GetLeft(), i);
+	if (tree->GetRight() != nullptr) ProcedureBlock (tree->GetRight(), i);
+	OperatorProcess(tree);
+	if (tree->GetData() == "exit")
+	{
+		cout << "jmp exit_proc_" << i << endl;
+		exit_flag_proc = 1;
+	}
 }
 
 void Generator::Start()
@@ -166,6 +235,10 @@ void Generator::Finish()
 	if (_const != nullptr) Constant(_const);
 	if (_var != nullptr) Variable(_var);
 	if (_procedure != nullptr) ProcedureTreeTraversal(_procedure);
+	for (int i = 0; i < pr_count_proc; i++)
+	{
+		if (procedure_address[i] != nullptr) Procedure(procedure_address[i]);
+	}
 	cout << ".code" << endl;		
 	cout << _name << ":" << endl;
 	cout << "mov ax, @Data" << endl;
@@ -174,6 +247,14 @@ void Generator::Finish()
 	if (exit_flag == 1) cout << "exit:" << endl;
 	cout << "mov ax,4C00h" << endl;
 	cout << "int 21h" << endl;
+	if (_procedure != nullptr)	//начинаем генерить процедуры
+	{
+		for (int i = 0; i < pr_count_proc; i++)
+		{
+			ProcedureProcess(procedure_address[i], i);
+			stack_out = "";
+		}
+	}
 	if ((ids->At(0).GetType() == 3))
 	{
 		cout << "end " << ids->At(0).GetContent() << endl;
--- Module/Generator.h
+++ Module/Generator.h
@@ -17,8 +17,13 @@ private:
 	void Operator(Tree* tree);
 	void OperatorProcess(Tree* tree);
 	void ProcedureTreeTraversal(Tree* tree);
+	void ProcedureAddress(Tree* tree);
+	void ProcedureAddressInside(Tree* tree);
 	void Procedure(Tree* tree);
-	void ProcedureProcess(Tree* tree);
+	void ProcedureProcess(Tree* tree, int i);
+	void ProcedureVar(Tree* tree);
+	void ProcedureVarProcess(Tree* tree);
+	void ProcedureBlock(Tree* tree, int i);
 	void Process(Tree* tree);
 	void Start();
 	void TreeTraversal(Tree* tree);
@@ -27,8 +32,17 @@ private:
 	Tree* _var;
 	Tree* _code;
 	Tree* _procedure;
+	//штуки для процедур
+	Tree* procedure_address[10];
+	Tree* var_proc[10];
+	Tree* block_proc[10];
+	int pr_count_proc = 0;
+	int pr_count_var = 0;
+	int pr_count_block = 0;
+	std::string stack_out = "";
 	std::string _name;
 	bool exit_flag = 0;
+	bool exit_flag_proc = 0;
 public:
 	Generator(List<ID>* ids, Tree* syntaxTree);
 	void Working();
